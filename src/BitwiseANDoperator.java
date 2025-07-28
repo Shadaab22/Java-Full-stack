77ee72615de18e5a3c5561281e74f72d5e7a9e62
Shadaab22 <shadaab0510@gmail.com>
2025-07-28 16:20:23 +0530

    Day 1 programs

---
+++ src/BitwiseANDoperator.java
@@ -0,0 +1,10 @@
+public class BitwiseANDoperator {
+
+	public static void main(String[] args) {
+		int x = 7;
+		int y = 9;
+		int z = x & y;
+		System.out.println("z = " + z);
+	}
+
+}
---
+++ src/BitwiseXORoperator.java
@@ -0,0 +1,11 @@
+public class BitwiseXORoperator {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 9;
+		int z = x ^ y;
+		System.out.println("z = " + z);
+
+	}
+
+}
