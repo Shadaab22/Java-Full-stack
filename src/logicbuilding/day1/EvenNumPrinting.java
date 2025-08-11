78243d19840697d3a634e10bf0f3b4dae3183cb0
Shadaab22 <shadaab0510@gmail.com>
2025-08-11 10:38:46 +0530

    Added Day1 Programs in logicbuilding

---
+++ src/logicbuilding/day1/EvenNumPrinting.java
@@ -0,0 +1,9 @@
+package logicbuilding.day1;
+
+public class EvenNumPrinting {
+  public static void main(String[] args) {
+    for (int i=2; i<=100; i=i+2){
+      System.out.println(i);
+    }
+  }
+}
---
+++ src/logicbuilding/day1/OddNumPrinting.java
@@ -0,0 +1,9 @@
+package logicbuilding.day1;
+
+public class OddNumPrinting {
+  public static void main(String[] args) {
+    for (int i=1; i<=99; i=i+2){
+      System.out.println(i);
+    }
+  }
+}
