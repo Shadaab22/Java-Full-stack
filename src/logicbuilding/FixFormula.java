c003a0161fbfaa1ca646e09ca35e2f4ab44123cf
Shadaab22 <shadaab0510@gmail.com>
2025-08-11 20:28:55 +0530

    Added new program in logicbuilding

---
+++ src/logicbuilding/FixFormula.java
@@ -0,0 +1,54 @@
+package logicbuilding;
+import java.util.*;
+
+public class FixFormula {
+  public static int fixTheFormula(String input) {
+
+    char[] ops = new char[input.length()];
+    int[] nums = new int[input.length()];
+    int opCount = 0, numCount = 0;
+
+
+    for (int i = 0; i < input.length(); i++) {
+      char ch = input.charAt(i);
+      if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+        ops[opCount++] = ch;
+      } 
+      else if (ch >= '0' && ch <= '9') {
+        nums[numCount++] = ch - '0';
+      }
+    }
+
+
+    int result = nums[0];
+    for (int i = 0; i < opCount; i++) {
+      switch (ops[i]) {
+        case '+':
+            result += nums[i + 1];
+            break;
+        case '-':
+            result -= nums[i + 1];
+            break;
+        case '*':
+            result *= nums[i + 1];
+            break;
+        case '/':
+            result /= nums[i + 1];
+            break;
+      }
+    }
+    return result;
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter a question : ");
+    String input = sc.nextLine();
+
+    System.out.println(fixTheFormula(input));
+    sc.close();
+    // System.out.println(fixTheFormula("ve8+you2-7to/*32")); // Output: 2
+    // System.out.println(fixTheFormula("1=+t5s-8th14birdS")); // Output: 35
+    // System.out.println(fixTheFormula("Fo+23the3*like2=")); // Output: 17
+  }
+}
