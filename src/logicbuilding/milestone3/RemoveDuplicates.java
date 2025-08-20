393e8a96a3f777658c80aab7fd238694fa27bd34
Shadaab22 <shadaab0510@gmail.com>
2025-08-20 22:01:35 +0530

    Added milestone3

---
+++ src/logicbuilding/milestone3/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+package logicbuilding.milestone3;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        String str = "programming";
+        StringBuilder result = new StringBuilder();
+        boolean[] seen = new boolean[256];
+
+        for (char c : str.toCharArray()) {
+            if (!seen[c]) {
+                seen[c] = true;
+                result.append(c);
+            }
+        }
+        System.out.println("Without duplicates: " + result);
+    }
+}
---
+++ src/logicbuilding/milestone3/TrappingRainWater.java
@@ -0,0 +1,25 @@
+package logicbuilding.milestone3;
+
+public class TrappingRainWater {
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int n = height.length;
+        
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        
+        leftMax = height;
+        for (int i = 1; i < n; i++)
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        
+        rightMax[n-1] = height[n-1];
+        for (int i = n-2; i >= 0; i--)
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        
+        int water = 0;
+        for (int i = 0; i < n; i++)
+            water += Math.min(leftMax[i], rightMax[i]) - height[i];
+        
+        System.out.println("Trapped Water: " + water);
+    }
+}
